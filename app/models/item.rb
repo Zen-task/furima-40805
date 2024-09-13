8b0cfe6056dae6682dda1df94e49c95639e0cfb5
Zen-task <gkhgkingus@gmail.com>
2024-09-13 20:44:43 +0900

    テストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,17 +11,15 @@ class Item < ApplicationRecord
   has_one_attached :image
   validates :image, presence: true
 
-  
   validates :name, presence: true
 
-  
   validates :description, presence: true
 
-  validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, 
+  validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id,
             numericality: { other_than: 1, message: "can't be blank" }
 
-  validates :price, presence: true, 
-                    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true, message: "は¥300~¥9,999,999の範囲内で入力してください" }
+  validates :price, presence: true,
+                    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true, message: 'は¥300~¥9,999,999の範囲内で入力してください' }
 
-  validates :price, numericality: { only_integer: true, message: "は半角数値で入力してください" }
+  validates :price, numericality: { only_integer: true, message: 'は半角数値で入力してください' }
 end
