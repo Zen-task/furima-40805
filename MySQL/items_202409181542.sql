ee6a35feb69c78630d42edf50aec5272fc5bd3a1
Zen-task <gkhgkingus@gmail.com>
2024-09-19 00:08:08 +0900

    詳細表示機能の実装

---
+++ MySQL/items_202409181542.sql
@@ -0,0 +1,3 @@
+INSERT INTO `furima-40805_development`.items (name,description,price,user_id,category_id,condition_id,shipping_fee_id,prefecture_id,shipping_day_id,created_at,updated_at) VALUES
+	 ('紫マントが引っかかった胸像','あいうえお',2222,3,8,7,2,4,4,'2024-09-18 06:06:04.029263','2024-09-18 06:06:04.116858'),
+	 ('高画質','あいうえお',2222,3,11,5,2,20,2,'2024-09-18 06:08:37.596479','2024-09-18 06:08:37.620164');
